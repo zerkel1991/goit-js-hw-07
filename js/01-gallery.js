d29a9bfaa93921ec10b901ef9894b9912b6e4cc3
zerkel1991 <acorus1991@gmail.com>
2022-07-12 17:31:41 +0300

    fff

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -4,26 +4,25 @@ import { galleryItems } from './gallery-items.js';
 
 
 const mainContainer = document.querySelector(".gallery");
-const frag = document.createDocumentFragment()
-for (let i = 0; i < galleryItems.length; i += 1) {
-    
-    const galleryItem = document.createElement("div");
-    galleryItem.classList.add("gallery__item");
-    frag.append(galleryItem);
-    const galleryLink = document.createElement("a");
-    galleryLink.classList.add("gallery__link");
-    galleryLink.href = galleryItems[i].original;
-    galleryItem.append(galleryLink);
-    const galleryImg = document.createElement("img");
-    galleryImg.classList.add("gallery__image")
-    galleryImg.dataset.source = galleryItems[i].original;
-    galleryImg.src = galleryItems[i].preview;
-    galleryImg.alt = galleryItems[i].description;
-    galleryLink.append(galleryImg);
- }
- mainContainer.append(frag);
 
- mainContainer.addEventListener('click',(event)=> {
+const imageEl = galleryItems.map(el=>{
+const galleryEl = `<div class="gallery__item">
+<a class="gallery__link" href=${el.original}>
+  <img
+    class="gallery__image"
+    src=${el.preview}
+    data-source=${el.original}
+    alt=${el.description}
+  />
+</a>
+</div>`
+return galleryEl
+})
+
+mainContainer.insertAdjacentHTML("beforeend",imageEl.join(" "))
+
+
+mainContainer.addEventListener('click',(event)=> {
     event.preventDefault()
     if (event.target.tagName !== "IMG"){
         return
@@ -35,5 +34,6 @@ for (let i = 0; i < galleryItems.length; i += 1) {
 instance.show()
 
 })
+
 
 
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,15 +1,14 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 const galleryEl = document.querySelector(".gallery");
-const frag = document.createDocumentFragment()
 
-galleryItems.forEach(element => {
+ const galleryArr = galleryItems.map(element => {
     const image = `<a class="gallery__item" href=${element.original}>
     <img class="gallery__image" src=${element.preview} alt=${element.description} />
   </a>`
-  galleryEl.insertAdjacentHTML("beforeend", image);
+  return image
 });
-galleryEl.append(frag)
+galleryEl.insertAdjacentHTML("beforeend",galleryArr.join(" "))
 
 var lightbox = new SimpleLightbox('.gallery a', {captionsData: "alt",captionPosition:"bottom",captionDelay : 250 });
 
